096a27b3e7f4668ddee12ab9ba00e2b9161af2ea
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-09-02 09:04:11 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: EvalExtension
+Plugin URI : https://github.com/TheMaxium69/EvalExtension
+Author: Maxime Tournier
+Author URI: https://tyrolium.fr
+Description: Extension WordPress, Extension de Voiture
+Version: 1.0-BETA
+*/
